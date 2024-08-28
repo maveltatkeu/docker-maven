5044006a738c6c59ce9368b26e5b7ce76faed9b3
mavel.tatkeu.mbiakop <mavel.tatkeu.mbiakop@accenture.com>
2024-08-28 18:44:18 +0400

    Updated  2nd commit

--- src/test/java/com/example/docker_maven/DockerMavenApplicationTests.java
+++ src/test/java/com/example/docker_maven/DockerMavenApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.docker_maven;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,9 +12,10 @@ class DockerMavenApplicationTests {
     public static Logger logger = LoggerFactory.getLogger(DockerMavenApplicationTests.class);
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
         logger.info("Test case executing...");
-        assertEquals(true, true);
+        logger.info("Test case executing second statements ...");
+        assertEquals(2, 1+1);
     }
 
 }
