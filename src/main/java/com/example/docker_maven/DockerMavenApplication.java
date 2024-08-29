ede3bd595d644596fe5cb61fc83ac7ea421a88c9
mavel.tatkeu.mbiakop <mavel.tatkeu.mbiakop@accenture.com>
2024-08-29 11:07:09 +0400

    Updated  4th commit

--- src/main/java/com/example/docker_maven/DockerMavenApplication.java
+++ src/main/java/com/example/docker_maven/DockerMavenApplication.java
@@ -5,16 +5,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
 public class DockerMavenApplication {
 
-    public static Logger logger = LoggerFactory.getLogger(DockerMavenApplication.class);
+    public static final Logger logger = LoggerFactory.getLogger(DockerMavenApplication.class);
 
     public static void main(String[] args) {
         logger.info("Application Executed");
         SpringApplication.run(DockerMavenApplication.class, args);
+    }
+
+    @GetMapping("/ping")
+    public String message() {
+        return "Wao!! Application Deployed successfully in Jenkins V..";
     }
 
     @PostConstruct
