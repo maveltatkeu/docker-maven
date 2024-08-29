0f9c1bbf363de154e8224458ad6e3ef39688a485
mavel.tatkeu.mbiakop <mavel.tatkeu.mbiakop@accenture.com>
2024-08-29 11:10:18 +0400

    Updated failed test

--- src/test/java/com/example/docker_maven/DockerMavenApplicationTests.java
+++ src/test/java/com/example/docker_maven/DockerMavenApplicationTests.java
@@ -15,7 +15,7 @@ class DockerMavenApplicationTests {
     void contextLoads() {
         logger.info("Test case executing...");
         logger.info("Test case executing second statements ...");
-        assertEquals(9, 5+4);
+        assertEquals(77, 5+4);
     }
 
 }
